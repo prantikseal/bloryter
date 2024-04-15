98017152de3e4518c4b59e9a1b40986c441d8fcf
prantikseal <83081442+prantikseal@users.noreply.github.com>
2024-04-15 21:59:24 +0530

    add: frontend updates

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,13 +1,20 @@
-
+import Tab from "./Tab";
 
 
 const HeroSection = () => {
+    const tabs = [
+        { title: 'Analyse Tone', content: <div>Content for Tab 1</div> },
+        { title: 'Generate Content', content: <div>Content for Tab 2</div> },
+        { title: 'Review Content', content: <div>Content for Tab 3</div> },
+    ];
     return (
         <section className=" text-white p-8 ">
             <div className="container mx-auto flex flex-col justify-center items-center">
                 <h1 className="text-6xl font-bold mb-12 text-center">Bloryter Simple Easy Fast</h1>
                 <p className="text-lg mb-4 text-center">Get blogs written in your own tone within seconds</p>
-                <a href='#app-section' className="text-lg mb-8">Get Started</a>
+            </div>
+            <div className="w-full mx-auto">
+                <Tab tabs={tabs} />
             </div>
         </section>
     );
---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const Tab = ({tabs}) => {
+    const [activeTab, setActiveTab] = useState(0);
+    const handleTabClick = (index) => {
+        setActiveTab(index);
+      };
+    return (
+        <div className="flex justify-center items-center flex-col p-4 bg-opacity-40 bg-black shadow-md shadow-slate-600 rounded-md">
+          <div className="flex gap-4 text-base md:text-xl font-semibold">
+            {tabs.map((tab, index) => (
+              <div
+                key={index}
+                className={`cursor-pointer p-2 rounded-md ${
+                  index === activeTab ? 'bg-black text-white' : 'bg-gray-200 text-black'
+                }`}
+                onClick={() => handleTabClick(index)}
+              >
+                {tab.title}
+              </div>
+            ))}
+          </div>
+          <div className="p-4">
+            {tabs[activeTab].content}
+          </div>
+
+        </div>
+      );
+}
+
+export default Tab
