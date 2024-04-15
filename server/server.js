e7cd8c26afafbb99521368a38a0048ab6463323c
prantikseal <83081442+prantikseal@users.noreply.github.com>
2024-04-16 03:48:55 +0530

    fix: cors errors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://blogryter.netlify.app/',
   credentials: true,
 }));
 app.use(middlewares);
